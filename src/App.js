a89a5564fb6b22bf677c429414ca2c7f7d20dd19
박상웅 <114065532+xxng1@users.noreply.github.com>
2023-10-13 18:59:06 +0900

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,139 @@
+import React from "react";
+import './App.css';
+import "./style.css";
+
+
+function App() {
+
+        return (
+            <div className="screen">
+                <div className="div">
+                    <div className="overlap">
+                        <div className="text-wrapper">상점</div>
+                        <img className="icon-shop-store" alt="Icon shop store" src="icon-shop-store.png" />
+                    </div>
+                    <div className="frame">
+                        <div className="overlap-group">
+                            <div className="div-wrapper">
+                                <div className="text-wrapper-2">구매하기</div>
+                            </div>
+                            <div className="text-wrapper-3">방문 인사말 보이스</div>
+                            <div className="overlap-2">
+                                <div className="text-wrapper-4">2,000</div>
+                                <img className="img" alt="Frame" src="frame.svg" />
+                            </div>
+                            <div className="overlap-3">
+                                <div className="text-wrapper-5">상세 설명</div>
+                            </div>
+                            <img className="image" alt="Image" src="image-9.png" />
+                        </div>
+                        <div className="overlap-group-2">
+                            <div className="overlap-4">
+                                <div className="text-wrapper-6">3,000</div>
+                                <div className="text-wrapper-7">가수 아이유의 AI 보이스</div>
+                                <img className="frame-2" alt="Frame" src="Ellipse 31.svg" />
+                            </div>
+                            <div className="overlap-3">
+                                <div className="text-wrapper-5">상세 설명</div>
+                            </div>
+                            <div className="overlap-5">
+                                <div className="text-wrapper-2">구매하기</div>
+                            </div>
+                            <img className="ellipse" alt="Ellipse" src="ellipse-30.jpg" />
+                        </div>
+                        <div className="overlap-6">
+                            <div className="overlap-3">
+                                <div className="text-wrapper-5">상세 설명</div>
+                            </div>
+                            <div className="overlap-5">
+                                <div className="text-wrapper-2">구매하기</div>
+                            </div>
+                            <div className="overlap-7">
+                                <div className="text-wrapper-4">3,000</div>
+                                <img className="img" alt="Frame" src="frame-2.svg" />
+                            </div>
+                            <div className="text-wrapper-8">배우 나나의 AI 보이스</div>
+                            <img className="ellipse-2" alt="Ellipse" src="ellipse-31.png" />
+                        </div>
+                        <div className="overlap-8">
+                            <img className="ellipse-3" alt="Ellipse" src="ellipse-32.png" />
+                            <div className="overlap-9">
+                                <div className="text-wrapper-9">3,000</div>
+                                <div className="text-wrapper-10">가수 성시경의 AI 보이스</div>
+                                <img className="frame-3" alt="Frame" src="frame-3.svg" />
+                            </div>
+                            <div className="overlap-3">
+                                <div className="text-wrapper-5">상세 설명</div>
+                            </div>
+                            <div className="overlap-5">
+                                <div className="text-wrapper-2">구매하기</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="frame-4">
+                        <div className="overlap-group">
+                            <div className="div-wrapper">
+                                <div className="text-wrapper-2">구매하기</div>
+                            </div>
+                            <div className="overlap-10">
+                                <div className="text-wrapper-11">스트릭 프리즈</div>
+                                <div className="text-wrapper-12">2,000</div>
+                                <img className="frame-5" alt="Frame" src="frame-4.svg" />
+                            </div>
+                            <div className="overlap-3">
+                                <div className="text-wrapper-5">상세 설명</div>
+                            </div>
+                            <img className="image-2" alt="Image" src="image-3.png" />
+                        </div>
+                        <div className="overlap-11">
+                            <div className="overlap-4">
+                                <div className="text-wrapper-13">400</div>
+                                <p className="text-wrapper-7">스트릭 그래프 염색칩 x 5</p>
+                                <img className="frame-2" alt="Frame" src="frame-5.svg" />
+                            </div>
+                            <img className="image-3" alt="Image" src="image-4.png" />
+                            <div className="overlap-3">
+                                <div className="text-wrapper-5">상세 설명</div>
+                            </div>
+                            <div className="overlap-5">
+                                <div className="text-wrapper-2">구매하기</div>
+                            </div>
+                        </div>
+                        <div className="overlap-6">
+                            <div className="overlap-3">
+                                <div className="text-wrapper-5">상세 설명</div>
+                            </div>
+                            <div className="overlap-5">
+                                <div className="text-wrapper-2">구매하기</div>
+                            </div>
+                            <div className="overlap-7">
+                                <div className="text-wrapper-4">3,000</div>
+                                <img className="img" alt="Frame" src="frame-6.svg" />
+                            </div>
+                            <p className="text-wrapper-8">스트릭 그래프 염색칩 x 40</p>
+                            <img className="image-4" alt="Image" src="image.png" />
+                        </div>
+                        <div className="overlap-8">
+                            <div className="overlap-12">
+                                <div className="text-wrapper-14">10,000</div>
+                                <div className="text-wrapper-10">「도심의 야경」프로필 배경</div>
+                                <img className="frame-6" alt="Frame" src="frame-7.svg" />
+                            </div>
+                            <div className="overlap-3">
+                                <div className="text-wrapper-5">상세 설명</div>
+                            </div>
+                            <img className="image-5" alt="Image" src="image-10.png" />
+                            <div className="overlap-5">
+                                <div className="text-wrapper-2">구매하기</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="text-wrapper-15">보유</div>
+                    <div className="text-wrapper-16">1,150 점</div>
+                </div>
+            </div>
+        );
+    };
+          
+  
+export default App;
