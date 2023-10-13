42910c675cdd2c6d699fbe2e91b9497db5df2ef5
hyeonyoung <ahy1612@naver.com>
2023-10-13 20:38:44 +0900

    merge yungi

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-<<<<<<< HEAD
 import {Route, Routes} from "react-router-dom";
 import MainPage from "./pages/MainPage";
 import MyblogPage from './pages/myblogpage';
 import NavBar from './Navigationbar';
 import Test from './pages/test'
+import Login from './login';
+import WritePost from './writePost'
 
 function App() {
   return (
@@ -14,16 +15,9 @@ function App() {
         <Route path={"/"} element={<MainPage />} />
         <Route path='/myblogpage' element={<MyblogPage />} />
         <Route path='/test' element={<Test />} />
+        <Route path='/writepost' element={<WritePost />} />
+        <Route path='/login' element={<Login />} />
       </Routes>
-=======
-import Login from './login';
-import WritePost from './writePost'
-function App() {
-  return (
-    <div className="App">
-     {/* <Login /> */}
-     <WritePost />
->>>>>>> origin/yungi
     </div>
   );
 }
