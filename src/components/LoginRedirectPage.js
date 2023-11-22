e33869316400c7d594e35ac24e3a13eb1d39722f
gwon477 <gwonsang247@gmail.com>
2023-11-22 11:51:17 +0900

    jwt

--- src/components/LoginRedirectPage.js
+++ src/components/LoginRedirectPage.js
@@ -14,22 +14,28 @@ const LoginRedirectPage = () => {
             // 받아온 code를 서버에 전달합니다.
             // 회원가입 & 로그인
             const response = await axios.get(`http://localhost:8080/oauth/login/${params.sns}?code=${code}`);
-            let dataList = [];
-            dataList = response.data; // 응답 데이터
+            // 응답 데이터
             // alert("로그인 성공: " + dataList)
             // console.log("두번째꺼가 null 인지?" + dataList[1])
             // console.log("마지막 id 값은??" + dataList[2])
-            if (dataList[1] === null) {
+            console.log(response.data);
+            console.log(response.data.email);
+            console.log(response.data.blogName);
+            console.log(response.data.memberId);
+
+            localStorage.setItem("userData",JSON.stringify(response.data));
+
+            if (response.data.blogName=== null) {
                 navigate("/signin1", {
                     state: {
-                        email: dataList[0],
-                        id: dataList[2]
+                        email: response.data.email,
+                        id: response.data.memberId
                     }
                 });
             } else {
                 // 이미 로그인 정보가 있으면 /sign1으로 가지 않고 바로 / 로 이동
                 navigate("/", {
-                    state: dataList[2]
+                    state: response.data.memberId
                 });
             }
         } catch (error) {
@@ -41,7 +47,8 @@ const LoginRedirectPage = () => {
     useEffect(() => {
         const searchParams = new URLSearchParams(location.search);
         // 버튼을 클릭했을 때 리디렉트 됬었던 http://localhost:8080/oauth/{kakao/nave/google} 로 부터 인가 코드(code)를 받아옵니다.
-        const code = searchParams.get('code');  // Redirect 시키면서 code를 쿼리 스트링으로 준다.
+        const code = searchParams.get('code'); 
+        console.log(code); // Redirect 시키면서 code를 쿼리 스트링으로 준다.
         if (code) {
             // alert("CODE = " + code)
             // console.log(code);
