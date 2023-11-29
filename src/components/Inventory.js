5305880bb4f658f906e65419430bcbef6135954a
hyeonyoung <ahy1612@naver.com>
2023-11-29 17:08:11 +0900

    feat: 인벤토리, 상점 상세 설명

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -32,14 +32,15 @@ function Inventory(props) {
     const userId = localUserData.memberId;
     const [userPoint, setuserPoint] = useState("");
     const [items, setItems] = useState([]);
+    const [itemDetail, setItemDetail] = useState();
+    const [modalOpen, setModalOpen] = useState(false);
 
     useEffect(() => {
         getpoint(userId);
         getInven(userId);
-        console.log(items.itemName);
     },[])
 
-    const getpoint = async(userId) => {
+    const getpoint = async (userId) => {
         try {
             const response = await StoreService.getPoint(userId); // Update the URL accordingly
             setuserPoint(response.data);
@@ -48,27 +49,19 @@ function Inventory(props) {
             setuserPoint("-");
         }
     }
-
-    const getInven = async(userId) => {
-        try{
+    const getInven = async (userId) => {
+        try {
             const response = await InvenService.getItems(userId);
             setItems(response.data);
-        }catch(error){
+        } catch (error) {
             console.error('Error fetching user data:', error);
         }
     }
 
-    function getItemImage (itemId){
-        // itemId와 매칭되는 이미지 파일 이름을 구한다. (예: store-item-1.svg)
-        if(itemId == '1'){return <img className={styles[`item-image`]} src={require(`../assets/store-item-1.svg`).default} />;} 
-        if(itemId == '2'){return <img className={styles[`item-image`]} src={require(`../assets/store-item-2.svg`).default} />;} 
-        if(itemId == '3'){return <img className={styles[`item-image`]} src={require(`../assets/store-item-3.svg`).default} />;} 
-        if(itemId == '4'){return <img className={styles[`item-image`]} src={require(`../assets/store-item-4.svg`).default} />;} 
-        if(itemId == '5'){return <img className={styles[`item-image`]} src={require(`../assets/store-item-5.svg`).default} />;} 
-        if(itemId == '6'){return <img className={styles[`item-image`]} src={require(`../assets/store-item-6.svg`).default} />;} 
-        if(itemId == '7'){return <img className={styles[`item-image`]} src={require(`../assets/store-item-7.svg`).default} />;} 
-          // 이미지를 렌더링한다.
-      };
+    const clickItemDetail = (item) => {
+        setModalOpen(true);
+        setItemDetail(item);
+    }
 
     return (
         <div className={styles[`common`]}>
@@ -86,18 +79,16 @@ function Inventory(props) {
                 {/*보유 아이템 리스트*/}
                 <div className={styles[`container`]}>
                     {items.map((item, index) => (
-                        <div key={index}>
+                        <div
+                            key={index}
+                            onClick={()=>{clickItemDetail(item)}}
+                        >
                             <div><strong>Name:</strong> {item.itemName}</div>
-                            
                             <button className={styles[`item`]}>
                                 <div className={styles[`item-image-container`]}>
-                                    {/* <img className={styles[`item-image`]}
-                                        src={require(`../assets/store-item-${item.itemId}.svg`).default}
+                                    <img className={styles[`item-image`]}
+                                        src={require(`../assets/store-item-${item.itemId}.svg`)}
                                     />
-                                    {/* <img className={styles[`item-image`]}
-                                        src={require(`../assets/store-item-1.svg`).default}
-                                    /> */}
-                                    {getItemImage(item.itemId)}
                                 </div>
                                 <div className={styles[`item-status-container`]}>
                                     <div className={styles[`item-title`]}>{item.itemName}</div>
@@ -108,65 +99,27 @@ function Inventory(props) {
                             </button>
                         </div>
                     ))}
-                    {/*첫번째 아이템*/}
-                    {/* <button className={styles[`item`]}>
-                        <div className={styles[`item-image-container`]}>
-                            <img className={styles[`item-image`]}
-                                 src={require("../assets/store-item-1.svg").default}
-                            />
-                        </div>
-                        <div className={styles[`item-status-container`]}>
-                            <div className={styles[`item-title`]}>방문 인사말 보이스</div>
-                            <div className={styles[`item-status`]}>
-                                <ItemStatus on={"true"}>사용중</ItemStatus>
-                            </div>
-                        </div>
-                    </button> */}
-                    {/*두번째 아이템*/}
-                    {/* <button className={styles[`item`]}>
-                        <div className={styles[`item-image-container`]}>
-                            <img className={styles[`item-image`]}
-                                 src={require("../assets/store-item-2.svg").default}
-                            />
-                        </div>
-                        <div className={styles[`item-status-container`]}>
-                            <div className={styles[`item-title`]}>스트릭 프리즈</div>
-                            <div className={styles[`item-status`]}>
-                                <ItemStatus>장착하기</ItemStatus>
-                            </div>
-                        </div>
-                    </button> */}
-                    {/* <button className={styles[`item`]}>
-                        <div className={styles[`item-image-container`]}>
-                            <img className={styles[`item-image`]}
-                                 src={require("../assets/store-item-3.svg").default}
-                            />
-                        </div>
-                    </button> */}
                 </div>
                 {/*해당 아이템 설명*/}
-                <ItemDetail />
+                {modalOpen && <ItemDetailModal item={itemDetail}/>}
             </div>
             
         </div>
     )
 }
 
-const ItemDetail=()=>{
+const ItemDetailModal=({item})=>{
     return (
         <div className={styles[`container`]}>
             <div className={styles[`item-image-container`]}>
                 <img className={styles[`item-image`]}
-                     src={require("../assets/store-item-1.svg").default}
+                     src={require(`../assets/store-item-${item.itemId}.svg`)}
                 />
             </div>
-            <div className={styles[`item-title`]}>방문 인사말 보이스</div>
+            <div className={styles[`item-title`]}>{item.itemName}</div>
             <div>1개 보유</div>
             <div className={styles[`item-docs`]}>
-                사용하면 블로그 방문자를 맞이하는 인사말을 자동 재생할 수 있습니다.
-                자신이 설정한 인사말의 문구를 선택한 보이스로 재생하게 됩니다.
-                기본 보이스는 자신의 목소리로, 방문 인사말 보이스를 사용한 순간 프로필에서 음량 조절 버튼이 보이게 되며
-                블로그 방문자에게 인사말 문구가 자동재생됩니다.
+                {item.itemInfo}
             </div>
         </div>
     )
